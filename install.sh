2641a6803e697cd00f3db9a738e8c84102f39c7e
Yassin Siouda <yassin@siouda.com>
2019-08-02 15:51:10 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+# General update
+sudo apt update
+sudo apt dist-upgrade
+
+
+# LAMP
+sudo apt install apache2 php libapache2-mod-php mysql-server php-mysql php-curl php-gd php-intl php-json php-mbstring php-xml php-zip
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.zip
+unzip phpMyAdmin-4.9.0.1-all-languages.zip -d /var/www/html/ 
+mv /var/www/html/phpMyAdmin-4.9.0.1-all-languages /var/www/html/phpmyadmin
+
+
+
