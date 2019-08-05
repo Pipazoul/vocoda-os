b292baba95342b05f53f179c9bd6b6ced0a2d5db
Yassin Siouda <yassin@siouda.com>
2019-08-05 10:05:58 +0200

    LAMP and composer install

--- install.sh
+++ install.sh
@@ -14,17 +14,32 @@ ntfs-3g -o rw,umask=0000 /dev/sda5 /mnt/win-data/
 
 # LAMP
 sudo apt install apache2 php libapache2-mod-php mysql-server php-mysql php-curl php-gd php-intl php-json php-mbstring php-xml php-zip
+sudo mysql_secure_installation
+
+echo "Mysql installation : enter the username you want for your mysql db : "
+read mysqlUser
+echo "and a password now : "
+read mysqlPassword
+
+sudo mysql -Bse "GRANT ALL PRIVILEGES ON *.* TO '"$mysqlUser"'@'localhost' IDENTIFIED BY '"$mysqlPassword"';"
+
+echo "User " $mysqlUser " created "
+
 wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.zip
-unzip phpMyAdmin-4.9.0.1-all-languages.zip -d /var/www/html/ 
-mv /var/www/html/phpMyAdmin-4.9.0.1-all-languages /var/www/html/phpmyadmin
+sudo unzip phpMyAdmin-4.9.0.1-all-languages.zip -d /var/www/html/ 
+sudo mv /var/www/html/phpMyAdmin-4.9.0.1-all-languages /var/www/html/phpmyadmin
+sudo a2enmod rewrite
+sudo service apache2 restart
+
 
 # Apps
 
 # VS code
 # Processing
 # Composer
-# Slack
-sudo apt install evolution f.lux git spotify firefox chrome nodejs npm docker python3 pip
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
 
 
 
